62c9f0640929cdeec8efdc8d73a8313c7b103fd7
richardcalata <calatar6@gmail.com>
2018-06-18 14:09:44 -0500

    third test

--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -1,9 +1,7 @@
 package com.codeup.blog;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostController {
@@ -12,5 +10,23 @@ public class PostController {
     @ResponseBody public String index(){
 
         return "posts index page";
+    }
+
+    @RequestMapping("/posts/{id}")
+    @ResponseBody public String post(@PathVariable long id){
+
+        return "An individual post";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String createPost(){
+        return "view the form for Creating a Post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String savePost() {
+        return "saving to the database";
     }
 }
